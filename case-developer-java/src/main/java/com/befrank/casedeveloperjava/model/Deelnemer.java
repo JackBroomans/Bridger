ce453e8699753b97fc0c1c9f6361d40e2a67b1d9
JackReadyEducation <jack.broomans.warrenaar@readyeducation.com>
2023-04-13 15:41:04 +0200

    initial setup

--- case-developer-java/src/main/java/com/befrank/casedeveloperjava/model/Deelnemer.java
+++ case-developer-java/src/main/java/com/befrank/casedeveloperjava/model/Deelnemer.java
@@ -3,6 +3,7 @@ package com.befrank.casedeveloperjava.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
@@ -13,16 +14,62 @@ public class Deelnemer {
     @GeneratedValue(strategy = IDENTITY)
     private Integer id;
 
-    private String familyName;
-    private String prefixesName;
-    private String foreNames;
-    private String initials;
-    private String titlesPrefix;
-    private String titlesSuffix;
-    private Geslacht gender;
+    private String familieNaam;
+    private String voorvoegsels;
+    private String voornamen;
+    private String initialen;
+    private String prefixTitels;
+    private String suffixTitels;
+
+    @Transient
+    private Geslacht geslacht;
+
+    private String codeGeslacht;
 
     public Integer getId() {
         return id;
+    }
+
+    public String getFamilieNaam() {
+        return familieNaam;
+    }
+    public void setFamilieNaam(String familyName) {
+        this.familieNaam = familyName;
+    }
+
+    public String getVoorvoegsels() {
+        return voorvoegsels;
+    }
+    public void setVoorvoegsels(String prefixesName) {
+        this.voorvoegsels = prefixesName;
+    }
+
+    public String getVoornamen() {
+        return voornamen;
+    }
+    public void setVoornamen(String foreNames) {
+        this.voornamen = foreNames;
+    }
+
+    public String getInitialen() {
+        return initialen;
+    }
+    public void setInitialen(String initials) {
+        this.initialen = initials;
+    }
+
+    public String getPrefixTitels() {
+        return prefixTitels;
+    }
+    public void setPrefixTitels(String titlesPrefix) {
+        this.prefixTitels = titlesPrefix;
+    }
+
+    public String getSuffixTitels() {
+        return suffixTitels;
+    }
+    public void setSuffixTitels(String titlesSuffix) {
+        this.suffixTitels = titlesSuffix;
     }
 
     /**
@@ -34,4 +81,8 @@ public class Deelnemer {
     public String formatFullName() {
         return "";
     }
+
+    /**
+     * <strong></strong>
+     */
 }
